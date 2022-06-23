cc06cba3a4019f0dde77148ab0ea49cedb65b6f1
prathamrasal <prathamrasal6@gmail.com>
2022-06-23 22:37:55 +0530

    Connected Contact Form's Backend

--- src/Components/Section3.jsx
+++ src/Components/Section3.jsx
@@ -1,40 +1,52 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useState } from 'react'
 import "../Style/Section3.css"
 
 const Section3 = () => {
+  const [user, setUser] = useState({ name: "", email: "", message: "" })
+
+  const onchange = (e) => {
+    setUser({ ...user, [e.target.name]: e.target.value })
+  }
+
+  const handleSubmit = async () => {
+    const { name, email, message } = user
+    const res = await axios.post('https://lobo-backend.herokuapp.com/contact', user);
+  }
+
   return (
     <div className='Section3'>
       <div className="container">
         <div className="left">
           <img src="https://s3-alpha-sig.figma.com/img/28df/a033/c2f86abe1de8bee39459788581881c12?Expires=1656892800&Signature=br7S4K89NVJgu693eyORF~2d0lSRvGXuS8kJ3KyLHspRn-cn36u~mFxaVDek4w8YxXPVd-0WHkCsqiD0JwbAmhzJp9FXyrwhKzANbsC8lbV6jPJHByLL0VQZJDz8a360ymC5TxgHVjr8seejhtaQHB59DjwgrEDqqCvlBRd0R3My-EWPFBDIh3PmUZeVfgiYOx-Qrqq6JhweHQOdx69L8ooQPKJnIvGnLP3fTWHEzAIsELrxvWmzZ8WjOak9o1evzemAMeb-S0c9i-6xCGfpM6P2mPFYHzB6iQs2gBzk5PGlH7oGWW9Wrn-zmEEfAwlW-PzqbTNR6JplGCtG6L7gUw__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA" alt="img" />
-          
+
         </div>
         <div className="right">
           <div className="heading">
             <h3>To get the booklist</h3>
             <h4>(kindly fill the form)</h4>
           </div>
-          <form>
+          <div>
             <div className="inputBoxes">
               <div className='labelBox'>
                 <label className='formLabel'>Full Name:</label>
-              </div>  
-              <input className='formInputs' type="text" minLength={3} required />
+              </div>
+              <input className='formInputs' onChange={onchange} name='name' type="text" minLength={3} required />
             </div>
             <div className="inputBoxes">
               <div className='labelBox'>
                 <label className='formLabel' minlength="4">Email Id:</label>
               </div>
-              <input className='formInputs' type="email" required />
+              <input className='formInputs' onChange={onchange} name='email' type="email" required />
             </div>
             <div className="inputBoxes">
               <div className='labelBox'>
                 <label className='formLabel'>Message (Optional)</label>
               </div>
-              <textarea cols="30" rows="4" className='formInputs meggage' ></textarea>
+              <textarea cols="30" rows="4" onChange={onchange} name='message' className='formInputs meggage' ></textarea>
             </div>
-            <button className='btn'> Submit</button>
-          </form>
+            <button className='btn' onClick={handleSubmit}> Submit</button>
+          </div>
         </div>
       </div>
       <div className="footer">
